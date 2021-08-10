be3e0edf5924cb9242860c30b8cf39901ea8eb20
Abhinavsuresh21 <88644349+Abhinavsuresh21@users.noreply.github.com>
2021-08-10 12:43:27 +0530

    Add files via upload

---
+++ arithmetic/arth_op.py
@@ -0,0 +1,16 @@
+a=int(input("Enter A value:"))
+b=int(input("Enter B value:"))
+Add=a+b;
+Sub=a-b;
+Mult=a*b;
+Div=a/b;
+Exp=a**b;
+print("\t\t output \n\n")
+print("Entered A value:",a)
+print("Entered B value:",b)
+print("\t\t arthamatic operations \n\n")
+print("calc Add value:",Add)
+print("calc Sub value:",Sub)
+print("calc Mult value:",Mult)
+print("calc Div value:",Div)
+print("calc Exp value:",Exp) 
