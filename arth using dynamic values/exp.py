6e41faa0f9a5d5f73efa2e2d2e15622c257ded4f
Abhinavsuresh21 <88644349+Abhinavsuresh21@users.noreply.github.com>
2021-08-10 12:45:19 +0530

    Add files via upload

---
+++ arth using dynamic values/exp.py
@@ -0,0 +1,5 @@
+a=int(input("Enter A value:"))
+b=int(input("Enter B value:"))
+c=a**b;
+print("\t\t output \n\n")
+print("calc Exp value:",c)
---
+++ arth using dynamic values/sub.py
@@ -0,0 +1,4 @@
+a=int(input("Enter A value:"))
+b=int(input("Enter A value:"))
+c=a-b;
+print(c)
