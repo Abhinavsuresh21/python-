dca3c768474e20e681cacce3ce96eaf111812ff0
Abhinavsuresh21 <88644349+Abhinavsuresh21@users.noreply.github.com>
2021-10-18 11:50:27 +0530

    Add files via upload

---
+++ pattern/pattern1.py
@@ -0,0 +1,12 @@
+rows = 6
+num=3
+for num in range(rows):
+      
+    for i in range(num):
+
+        print(num, end=" ") # print number
+
+    # line after each row to display pattern correctly
+
+    print(" ")
+
